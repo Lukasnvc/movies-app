54795dfe59521db2b718a27221f1da4e326caf06
Lukasnvc <lukasnvc@gmail.com>
2023-05-31 13:25:17 +0300

    responsive and gPages left

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,36 @@
 import "./Header.scss"
 
-import { Link } from 'react-router-dom'
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchAsyncMovies, fetchAsyncShows } from "../../features/movies/movieSlice";
+
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Logo from '../../common/logo.png'
+import { useDispatch } from "react-redux";
 import user from "../../images/user.png";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const [search, setSearch] = useState('Search')
+  const [text, setText] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(text === '') return setSearch('Type seach here ...') 
+    dispatch(fetchAsyncMovies(text))
+    dispatch(fetchAsyncShows(text))
+    setText('')
+    setSearch('Search')
+  }
   return (
     <div className='header'>
-      <Link to="/">
-      <div className="logo">MOVIE APP</div>
-      </Link>
+     
+     <Link to="/" className="logo"><img src={Logo} alt="" /></Link>
+      <div className="search-bar">
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={text} placeholder={search} onChange={ (e) => setText(e.target.value)} />
+          <button type="submit"><FaSearch/></button>
+        </form>
+      </div>
      
       <div className='user-image'>
         <img src={user} alt="user image" />
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { fetchAsyncMovies, fetchAsyncShows } from '../../features/movies/movieSlice';
+import { randomMovie, randomShow } from '../../common/randomPick';
 
 import MovieListing from '../MovieListing/MovieListing';
 import { useDispatch } from 'react-redux';
@@ -7,8 +8,8 @@ import { useDispatch } from 'react-redux';
 const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchAsyncMovies())
-    dispatch(fetchAsyncShows())
+    dispatch(fetchAsyncMovies(randomMovie))
+    dispatch(fetchAsyncShows(randomShow))
   }, [dispatch]);
 
   return (
